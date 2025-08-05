3234e38d75c22519bc31a6a7c930c1b6873da0c5
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2025-08-05 12:16:18 +0600

    passport authentication files added

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -8,7 +8,7 @@ const companyRouter = Router();
 const companyController = container.get<CompanyController>(TYPES.CompanyController);
 
 companyRouter.get('/:id', companyController.getById.bind(companyController));
-companyRouter.post('/', companyController.createCompany.bind(companyController));
+companyRouter.post('/registerCompany', companyController.createCompany.bind(companyController));
 companyRouter.put('/:id', companyController.updateCompany.bind(companyController));
 companyRouter.delete('/:id', companyController.deleteCompany.bind(companyController));
 
